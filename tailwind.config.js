3196e115fde63f7245d417a91d89e2bc8891ee1e
Nate Berkopec <nate.berkopec@gmail.com>
2021-01-28 13:35:09 -0700

    Github actions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ module.exports = {
   purge: [
     './src/**/*.html'
   ],
-   darkMode: false, // or 'media' or 'class'
-   theme: {
-     extend: {},
-   },
-   variants: {},
-   plugins: [
-     require('@tailwindcss/forms')
-   ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
  }
